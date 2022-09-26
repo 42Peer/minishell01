d281d985c3ad03af1e5211a53f6cff3a82d68d54
myTed <tribune11@nate.com>
2022-09-26 15:03:15 +0900

    노미네트 정리중

--- srcs/tree_parser.c
+++ srcs/tree_parser.c
@@ -4,31 +4,18 @@ char	**func_heredoc(t_node *node, char *delimiter, int quoted)
 {
 	int		fd;
 	int		i;
-	char 	*filename; //
+	char	*filename;
 	char	*str;
-	char	*expanded_str;
+	char	*expnd_str;
 
-	// (1-1) 히어독 여러개 잇을때 다른 파일이름 만들기
-	// (1-2) 나중에 실행파트에서 히어독 실행하려면 트리구조에 파일이름을 저장해놔야하는지?
 	fd = -1;
 	filename = ft_strdup("/tmp/here_doc_0");
-	// (1) tmpfile 만들기
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0666);
 	while (fd == -1)
 	{
 		filename = save(filename, '0', ft_strlen(filename));
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0666);
 	}
-	// (2) tmpfile에 히어독 받고 확장 유무체크후 처리
-
-	// 1. 델리미터 들고 오기
-	// 2. 한라인 읽기 (str = readline. 이때 ">" 출력)
-	// 3. 델리미터 인지 확인 
-	// 3-1. 델리미터면 반복문 나감 (if str == 델리미터)
-	// 4. 확장 필요하면 확장 라인 들고옴 (else)
-	// 5. 파일에 쓰기 (개행 붙이기)
-	// 6. 2번으로 간다
-
 	while (1)
 	{
 		expanded_str = ft_strdup("");
@@ -41,7 +28,7 @@ char	**func_heredoc(t_node *node, char *delimiter, int quoted)
 			while (str[++i])
 			{
 				if (str[i] == '$')
-					expanded_str = ft_strjoin(expanded_str, dollar_sign(str, &i));
+					expnded_str = ft_strjoin(expanded_str, dollar_sign(str, &i));
 				else
 					expanded_str = save(expanded_str, str[i], ft_strlen(expanded_str));
 			}
@@ -59,12 +46,9 @@ char	**func_heredoc(t_node *node, char *delimiter, int quoted)
 	free(str);
 	free(expanded_str);
 	close(fd);
-	// (3) 노드 갱신
-	// (3-1) << 노드의 content 의 <로 바꿔주기
 	if (node->content)
 		free(node->content);
 	node->content = ft_strdup("<");
-	// (3-2) << 노드의 DELIMITER를 heredoc 임시파일로 바꿔주기
 	if (node->right->content)
 		free(node->right->content);
 	node->right->content = filename;
@@ -92,7 +76,7 @@ void	redir_parser(t_node *node, int *flag)
 	int		quoted;
 	char	*new_content;
 
-	quoted = 0;		// 기본 0 = 인용 없음 으로 초기화
+	quoted = 0;		
 	if (!node)
 		return ;
 	if (node->type == T_REDIR)
@@ -106,10 +90,8 @@ void	redir_parser(t_node *node, int *flag)
 			set_or_get_status(258);
 			*flag = 1;
 		}
-		if (ft_strncmp(node->content, "<<", 3) == 0)	// heredoc이면
+		if (ft_strncmp(node->content, "<<", 3) == 0)
 		{
-			// printf("heredoc 도착!\n");
-			// printf("delimiter : %s\n", node->right->content);
 			new_content = delquote(node->right->content, &quoted);
 			free(node->right->content);
 			node->type = T_HEREDOC;
@@ -119,8 +101,8 @@ void	redir_parser(t_node *node, int *flag)
 	}
 	else
 	{
-		redir_parser(node->right, flag);	// T_REDIR
-		redir_parser(node->left, flag);	// 다른 redir
+		redir_parser(node->right, flag);
+		redir_parser(node->left, flag);
 	}
 }
 
@@ -128,7 +110,6 @@ int	tree_parser(t_node *node, int *flag)
 {
 	if (!(node))
 		return (0);
-	// printf("now node type : %s node str : %s\n", token_str[node->type], node->str);
 	if (node->type == N_PHRASE)
 	{
 		redir_parser(node->left, flag);
@@ -136,8 +117,8 @@ int	tree_parser(t_node *node, int *flag)
 	}
 	else
 	{
-		tree_parser(node->left, flag);	// phrase
-		tree_parser(node->right, flag);	// 새 프로세스
+		tree_parser(node->left, flag);
+		tree_parser(node->right, flag);
 	}
 	return (0);
 }
