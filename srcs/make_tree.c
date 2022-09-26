f44747d813cfa4eab0ac6f91fae2a26b1792ec59
joonseong11 <jwjsjwnsstjd@gmail.com>
2022-09-26 14:49:05 +0900

    after do modulize make_tree

--- srcs/make_tree.c
+++ srcs/make_tree.c
@@ -1,19 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   make_tree.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: sumsong <sumsong@student.42seoul.kr>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/08 13:53:16 by sumsong           #+#    #+#             */
-/*   Updated: 2022/09/24 20:44:31 by sumsong          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-// main 문에서 make_tree 어떻게 되어있는지
-// root = make_tree(head_token); // 토큰을 자료구조에 넣는다
-
 #include "../minishell.h"
+
+void	make_io_redir_node(t_node **io_redir)
+{
+	*io_redir = ft_calloc(1, sizeof(t_node));
+	if (!(*io_redir))
+		system_call_error(ALLOC_FAIL);
+	(*io_redir)->type = N_REDIR;
+}
+
+t_node	*attach_node_to_tree(t_node *cur_node, t_node *io_redir)
+{
+	while (cur_node->left)
+		cur_node = cur_node->left;
+	cur_node->left = io_redir;
+	cur_node = cur_node->left;
+	return (cur_node);
+}
+
+void	make_io_redir_child_node(t_node *cur_node, t_node **new_node)
+{
+	*new_node = ft_calloc(1, sizeof(t_node));
+	if (!(*new_node))
+		system_call_error(ALLOC_FAIL);
+	cur_node->right = *new_node;
+}
+
+void	make_io_redir_chile_node_content(
+	t_node *new_node, t_token **cur_token, t_node **cur_node)
+{
+	new_node->content = ft_strdup((*cur_token)->content);
+	*cur_token = (*cur_token)->next;
+	*cur_node = (*cur_node)->right;
+}
 
 t_token	*make_redir_node(t_node *cur_process, t_token *cur_token, int *flag)
 {
@@ -22,22 +40,13 @@ t_token	*make_redir_node(t_node *cur_process, t_token *cur_token, int *flag)
 	t_node	*new_node;
 	int		i;
 
-	io_redir = ft_calloc(1, sizeof(t_node));
-	if (!io_redir)
-		system_call_error(ALLOC_FAIL);
-	io_redir->type = N_REDIR;
+	make_io_redir_node(&io_redir);
 	cur_node = cur_process->left;
-	while (cur_node->left)
-		cur_node = cur_node->left;
-	cur_node->left = io_redir;
-	cur_node = cur_node->left;
+	cur_node = attach_node_to_tree(cur_node, io_redir);
 	i = -1;
 	while (++i < 2)
 	{
-		new_node = ft_calloc(1, sizeof(t_node));
-		if (!new_node)
-			system_call_error(ALLOC_FAIL);
-		cur_node->right = new_node;
+		make_io_redir_child_node(cur_node, &new_node);
 		if (!cur_token)
 		{
 			printf("Error: syntax error!\n");
@@ -47,9 +56,7 @@ t_token	*make_redir_node(t_node *cur_process, t_token *cur_token, int *flag)
 		}
 		else
 			new_node->type = cur_token->type;
-		new_node->content = ft_strdup(cur_token->content);
-		cur_token = cur_token->next;
-		cur_node = cur_node->right;
+		make_io_redir_chile_node_content(new_node, &cur_token, &cur_node);
 	}
 	return (cur_token);
 }
