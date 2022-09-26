46a1013370f5b3e68e6319215f7c9421347643c1
sumnii <sumsong@student.42seoul.kr>
2022-09-26 14:16:08 +0900

    [NORM] modularizing tokenize.c

--- srcs/tokenize.c
+++ srcs/tokenize.c
@@ -1,5 +1,15 @@
 #include "../minishell.h"
 
+void	info_init(t_token_info *info)
+{
+	info->start_idx = 0;
+	info->cur_idx = 0;
+	info->sin_quoted = 0;
+	info->dou_quoted = 0;
+	info->token_type = NONE;
+	info->token_list = NULL;
+}
+
 int	is_operator(char c)
 {
 	return (c == '>' || c == '<' || c == '|');
@@ -34,87 +44,110 @@ int	make_token(char *str, int copy_idx, t_token_info *info)
 	return (0);
 }
 
+void	case_part_of_operator(char *str, t_token_info *info)
+{
+	if (is_operator(str[info->cur_idx]))
+		make_token(str, info->cur_idx, info);
+	else
+	{
+		make_token(str, info->cur_idx - 1, info);
+		if (!is_whitespace(str[info->cur_idx]))
+		{
+			info->start_idx = info->cur_idx;
+			info->token_type = T_WORD;
+		}
+	}
+}
+
+void	case_quote_start(char chr, t_token_info *info)
+{
+	if (chr == '\'')
+		info->sin_quoted = 1;
+	else if (chr == '"')
+		info->dou_quoted = 1;
+	if (info->token_type == NONE)
+	{
+		info->start_idx = info->cur_idx;
+		info->token_type = T_WORD;
+	}
+}
+
+void	case_operator(char *str, t_token_info *info)
+{
+	if (info->token_type != NONE)
+		make_token(str, info->cur_idx - 1, info);
+	info->start_idx = info->cur_idx;
+	if (str[info->cur_idx] == '|')
+		info->token_type = T_PIPE;
+	else
+		info->token_type = T_REDIR;
+}
+
+void	case_blank(char *str, t_token_info *info)
+{
+	if (info->token_type != NONE)
+		make_token(str, info->cur_idx - 1, info);
+}
+
+void	case_part_of_word(char *str, t_token_info *info)
+{
+	if (info->sin_quoted && str[info->cur_idx] == '\'')
+		info->sin_quoted = 0;
+	else if (info->dou_quoted && str[info->cur_idx] == '"')
+		info->dou_quoted = 0;
+}
+
+void	case_new_word(t_token_info *info)
+{
+	info->start_idx = info->cur_idx;
+	info->token_type = T_WORD;
+}
+
+int	is_unquoted(t_token_info *info)
+{
+	if (info->sin_quoted || info->dou_quoted)
+	{
+		printf("WARNING: syntax error! unquoted!\n");
+		set_or_get_status(SYNTAX_ERROR);
+		cleaner(NULL, NULL, info->token_list);
+		return (1);
+	}
+	return (0);
+}
+
+void	token_search(char *str, t_token_info *info)
+{
+	if ((!info->sin_quoted && !info->dou_quoted)
+		&& ((info->token_type == T_REDIR || info->token_type == T_PIPE)))
+		case_part_of_operator(str, info);
+	else if ((!info->sin_quoted && !info->dou_quoted)
+		&& is_quote(str[info->cur_idx]))
+		case_quote_start(str[info->cur_idx], info);
+	else if ((!info->sin_quoted && !info->dou_quoted)
+		&& is_operator(str[info->cur_idx]))
+		case_operator(str, info);
+	else if ((!info->sin_quoted && !info->dou_quoted)
+		&& is_whitespace(str[info->cur_idx]))
+		case_blank(str, info);
+	else if (info->token_type == T_WORD)
+		case_part_of_word(str, info);
+	else
+		case_new_word(info);
+}
+
 t_token	*tokenize(char *str)
 {
 	t_token_info	info;
 
-	info.start_idx = 0;
-	info.cur_idx = 0;
-	info.sin_quoted = 0;
-	info.dou_quoted = 0;
-	info.token_type = NONE;
-	info.token_list = NULL;
-
+	info_init(&info);
 	while (str[info.cur_idx])
 	{
-		if ((!info.sin_quoted && !info.dou_quoted) && ((info.token_type == T_REDIR || info.token_type == T_PIPE)))
-		{ // 2~3 인용중이지 않고, 이전 문자가 연산자의 일부
-			if (is_operator(str[info.cur_idx]))
-			{
-				make_token(str, info.cur_idx, &info); // <- add_list_token();
-			}
-			else
-			{
-				make_token(str, info.cur_idx - 1, &info);
-				if (!is_whitespace(str[info.cur_idx]))
-				{
-					info.start_idx = info.cur_idx;
-					info.token_type = T_WORD;
-				}
-			}
-		}
-		else if ((!info.sin_quoted && !info.dou_quoted) && is_quote(str[info.cur_idx]))
-		{  // 4 인용중이지 않고, 인용시작
-			if (str[info.cur_idx] == '\'')
-				info.sin_quoted = 1;
-			else if (str[info.cur_idx] == '"')
-				info.dou_quoted = 1;
-			if (info.token_type == NONE)
-			{
-				info.start_idx = info.cur_idx;
-				info.token_type = T_WORD;
-			}
-		}
-		else if ((!info.sin_quoted && !info.dou_quoted) && is_operator(str[info.cur_idx]))
-		{ // 6 인용중이지 않고, 현재 인덱스가 연산자
-			if (info.token_type != NONE)
-				make_token(str, info.cur_idx - 1, &info);
-			info.start_idx = info.cur_idx;
-			if (str[info.cur_idx] == '|')
-				info.token_type = T_PIPE;
-			else
-				info.token_type = T_REDIR;
-		}
-		else if ((!info.sin_quoted && !info.dou_quoted) && is_whitespace(str[info.cur_idx]))
-		{ // 8 인용중이지 않고, 현재 인덱스가 blank
-			if (info.token_type != NONE)
-				make_token(str, info.cur_idx - 1, &info);
-		}
-		else if (info.token_type == T_WORD)
-		{ // 9 이전 문자가 단어의 한 부분일 때
-			if (info.sin_quoted && str[info.cur_idx] == '\'')
-				info.sin_quoted = 0;
-			else if (info.dou_quoted && str[info.cur_idx] == '"')
-				info.dou_quoted = 0;
-		}
-		else
-		{ // 11 현재 문자가 새로운 단어의 시작
-			info.start_idx = info.cur_idx;
-			info.token_type = T_WORD;
-		}
+		token_search(str, &info);
 		++info.cur_idx;
 	}
-	if (info.sin_quoted || info.dou_quoted)
-	{
-		printf("WARNING: syntax error! non-quoted!\n");
-		set_or_get_status(SYNTAX_ERROR);
-		cleaner(NULL, NULL, info.token_list);
+	if (is_unquoted(&info))
 		return (NULL);
-	}
-	if (info.token_type != NONE) // 1 eof or NULL을 만나면 
+	if (info.token_type != NONE)
 		make_token(str, info.cur_idx - 1, &info);
 	return (info.token_list);
 }
-
-// make_token에서 token_type == NONE일 때 아무것도 저장하지 않게 처리.
-// make_token 내부에서 token_type을 NONE으로 초기화.
