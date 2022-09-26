f3f4c35420e60d60c51c538bbe1946a92b027712
jnkeniaem <jeanne.kim9@gmail.com>
2022-09-26 20:47:03 +0900

    main 좀 더 줄임

--- srcs/main.c
+++ srcs/main.c
@@ -9,6 +9,16 @@ void	main_init(int *p_flag, t_struct *p_ds, int argc, char ***p_argv)
 		(void)(*p_argv);
 }
 
+void	about_prompt(char **p_str)
+{
+	signal_handler();
+	*p_str = readline("minishell > ");
+	if (!(*p_str))
+		exit(130);
+	in_process_signal_handler();
+	add_history(*p_str);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char		*str;
@@ -19,19 +29,13 @@ int	main(int argc, char **argv, char **envp)
 	make_env_array(envp);
 	while (1)
 	{
-		signal_handler();
-		str = readline("minishell > ");
-		if (!str)
-			exit(130);
-		in_process_signal_handler();
-		add_history(str);
+		about_prompt(&str);
 		ds.head_token = tokenize(str);
 		if (ds.head_token == NULL)
 		{
 			free(str);
 			continue ;
 		}
-		// ft_lstiter(ds.head_token, print_content);
 		if (make_tree(&ds) == 0
 			|| (!tree_parser(ds.root_node, &flag) && flag == 1))
 		{
