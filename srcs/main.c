2170f72c2fcb2995ed6a4a7827528ccbf5339973
myTed <tribune11@nate.com>
2022-10-04 01:04:01 +0900

    함수 이름 및 변수 t_head 수정

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,11 @@
 #include "../minishell.h"
 
 void	main_init(
-	int *syntx_erred, t_struct *p_ds, int argc, char **argv)
+	int *syntx_erred, t_head *p_head, int argc, char **argv)
 {
 	*syntx_erred = 0;
-	p_ds->head_token = NULL;
-	p_ds->root_node = NULL;
+	p_head->token = NULL;
+	p_head->node = NULL;
 	(void)argc;
 	(void)argv;
 }
@@ -20,50 +20,50 @@ void	about_prompt(char **p_str)
 	add_history(*p_str);
 }
 
-void	tree_parser_error(char *str, t_struct *ds, int *syntax_erred)
+void	tree_parser_error(char *str, t_head *p_head, int *syntax_erred)
 {
 	if (*syntax_erred)
 		printf("Error: syntax error!\n");
-	cleaner(str, ds, NULL);
+	cleaner(str, p_head, NULL);
 	*syntax_erred = 0;
 }
 
-int	loop_shell_prompt(t_struct *ds, int *p_syntx_erred)
+int	loop_shell_prompt(t_head *p_head, int *p_syntx_erred)
 {
-	char	*str;
+	char	*user_input_str;
 
 	while (1)
 	{
-		about_prompt(&str);
-		ds->head_token = tokenize(str);
-		if (ds->head_token == NULL)
+		about_prompt(&user_input_str);
+		p_head->token = tokenize(user_input_str);
+		if (p_head->token == NULL)
 		{
-			free(str);
+			free(user_input_str);
 			continue ;
 		}
-		if (make_tree(ds) == ERROR
-			|| (!tree_parser(ds->root_node, p_syntx_erred) && *p_syntx_erred))
+		if (make_tree(p_head) == ERROR
+			|| (!tree_parser(p_head->node, p_syntx_erred) && *p_syntx_erred))
 		{
-			tree_parser_error(str, ds, p_syntx_erred);
+			tree_parser_error(user_input_str, p_head, p_syntx_erred);
 			continue ;
 		}
 		if (set_or_get_heredoc_status(GET) == CTRL_C)
 		{
-			cleaner(str, ds, NULL);
+			cleaner(user_input_str, p_head, NULL);
 			continue ;
 		}
-		execute(ds);
-		cleaner(str, ds, NULL);
+		execute(p_head);
+		cleaner(user_input_str, p_head, NULL);
 	}
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_struct	ds;
+	t_head		head;
 	int			syntx_erred;
 
-	main_init(&syntx_erred, &ds, argc, argv);
+	main_init(&syntx_erred, &head, argc, argv);
 	make_g_env_array(envp);
-	loop_shell_prompt(&ds, &syntx_erred);
+	loop_shell_prompt(&head, &syntx_erred);
 	return (0);
 }
